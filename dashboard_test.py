20ccf73952b1f0757a6c1adf2a488d8cbb235d43
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2023-12-07 13:45:40 -0600

    dropdows working correctly

--- dashboard_test.py
+++ dashboard_test.py
@@ -7,25 +7,28 @@ data_frame = read_csv('Modelo coma.csv', delimiter=',', encoding='utf-8')
 
 #print rows where column '2-Participación abierta de los agentes sociales' is equal to '2-Participación abierta de los agentes sociales'
 
-'''
-Filter example
 
-filtered_rows = data_frame[data_frame['2-Participación abierta de los agentes sociales '] == '2-Participación abierta de los agentes sociales ']
+
+filtered_rows = data_frame[
+    (data_frame['2-Participación abierta de los agentes sociales '] == '2-Participación abierta de los agentes sociales ') &
+    (data_frame['Ciencia Ciudadana'] == 'Ciencia Ciudadana')
+]
 print(filtered_rows)
-'''
+
+
 
 #filter with regex
 #print('Columns -> ',data_frame.columns[data_frame.columns.str.match(r'\d+')])
 
 
-excluded_columns = ['Pais', 'Nombre de la iniciativa', 'Descripción', 'Sitio web', 'Correo contacto']
+#excluded_columns = ['Pais', 'Nombre de la iniciativa', 'Descripción', 'Sitio web', 'Correo contacto']
 
 #rest of columns (will be displayed in different dopdowns depending on the column value)
-rest_columns = data_frame.columns[5:]
+#rest_columns = data_frame.columns[5:]
 
 #columns for the first categories dropdown
-categories_dropdown = rest_columns[rest_columns.str.match(r'\d+')]
-print(categories_dropdown)
+#categories_dropdown = rest_columns[rest_columns.str.match(r'\d+')]
+#print(categories_dropdown)
 
 
 
